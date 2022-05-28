6bc77e8dd66cec869325d763cbcddc6bd9881b4e
André Pereira <andrepereira97@yahoo.com>
2022-05-28 20:31:23 -0300

    :sparkles: add calculator-react project

---
+++ calculator-react/src/components/Button.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './Button.css';
+
+export default (props) => (
+    <button
+        onClick={() => props.click && props.click(props.label)}
+        className={`
+    button 
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+    `}
+    >
+        {props.label}
+    </button>
+);
