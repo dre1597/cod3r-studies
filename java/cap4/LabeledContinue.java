30f0012643c62cd55c8fd5ed99365b81c9e10e55
André Pereira <andrepereira97@yahoo.com>
2022-09-06 21:24:43 -0300

    :sparkles: add some lessons from cap4

---
+++ java/cap4/LabeledContinue.java
@@ -0,0 +1,15 @@
+public class LabeledContinue {
+  public static void main(String[] args) {
+    outer:
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (i == 1) {
+          continue outer;
+        }
+        System.out.printf("[i:%d j:%d] ", i, j);
+      }
+      System.out.println();
+    }
+    System.out.println("End");
+  }
+}
